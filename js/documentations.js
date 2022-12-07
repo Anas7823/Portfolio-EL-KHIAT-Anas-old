eafc05e16dd5869d1cf759bff183affd7dcd7762
Anas7823 <anaselkhiat78@gmail.com>
2022-12-07 22:59:31 +0100

    page doc

---
+++ js/documentations.js
@@ -0,0 +1,125 @@
+$(document).ready(function()
+{
+     console.log("Le document est prêt");
+     $('.1').click(function()
+		{
+		console.log("Le bouton a été cliqué.");
+        if ($('.1').val()==='cacher')
+        {
+            $('#masquer1').fadeOut();
+            $('.1').val('montrer')
+		}
+        else
+        {
+            console.log("Le bontoun a été cliqué.");
+            $('#masquer1').show();
+            $('.1').val('cacher')
+        }
+    })
+});
+
+$(document).ready(function()
+{
+     console.log("Le document est prêt");
+     $('.2').click(function()
+		{
+		console.log("Le bouton a été cliqué.");
+        if ($('.2').val()==='cacher')
+        {
+            $('#masquer2').fadeOut();
+            $('.2').val('montrer')
+		}
+        else
+        {
+            console.log("Le bontoun a été cliqué.");
+            $('#masquer2').show();
+            $('.2').val('cacher')
+        }
+    })   
+});
+
+$(document).ready(function()
+{
+     console.log("Le document est prêt");
+     $('.3').click(function()
+		{
+		console.log("Le bouton a été cliqué.");
+        if ($('.3').val()==='cacher')
+        {
+            $('#masquer3').fadeOut();
+            $('.3').val('montrer')
+		}
+        else
+        {
+            console.log("Le bontoun a été cliqué.");
+            $('#masquer3').show();
+            $('.3').val('cacher')
+        }
+    })    
+});
+
+$(document).ready(function()
+{
+     console.log("Le document est prêt");
+     $('.4').click(function()
+		{
+		console.log("Le bouton a été cliqué.");
+        if ($('.4').val()==='cacher')
+        {
+            $('#masquer4').fadeOut();
+            $('.4').val('montrer')
+		}
+        else
+        {
+            console.log("Le bontoun a été cliqué.");
+            $('#masquer4').show();
+            $('.4').val('cacher')
+        }
+    })    
+});
+
+$(document).ready(function()
+{
+     console.log("Le document est prêt");
+     $('.5').click(function()
+		{
+		console.log("Le bouton a été cliqué.");
+        if ($('.5').val()==='cacher')
+        {
+            $('#masquer5').fadeOut();
+            $('.5').val('montrer')
+		}
+        else
+        {
+            console.log("Le bontoun a été cliqué.");
+            $('#masquer5').show();
+            $('.5').val('cacher')
+        }
+    })
+    
+        console.log("La mise en place est finie. En attente d'événements...");
+    
+});
+
+$(document).ready(function()
+{
+     console.log("Le document est prêt");
+     $('.6').click(function()
+		{
+		console.log("Le bouton a été cliqué.");
+        if ($('.6').val()==='cacher')
+        {
+            $('#masquer6').fadeOut();
+            $('.6').val('montrer')
+		}
+        else
+        {
+            console.log("Le bontoun a été cliqué.");
+            $('#masquer6').show();
+            $('.6').val('cacher')
+        }
+    })
+    
+        console.log("La mise en place est finie. En attente d'événements...");
+    
+});
